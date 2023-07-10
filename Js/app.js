9a9d15bf05a1c20e314b6be4cc99559592d5c9f9
unknown <chizaanicet@gmail.com>
2023-07-10 20:06:35 +0200

    add to my repository

--- Js/app.js
+++ Js/app.js
@@ -1,17 +1,22 @@
 const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const inputBox = document.querySelector('.search input');
 
-async function checkweather (city){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const inputBox = document.querySelector('.input');
+const inputBtn = document.querySelector('.search button');
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Map.round(data.main.temp) + '°c';
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 }
-checkweather ();
+
+inputBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value); 
+})
 
