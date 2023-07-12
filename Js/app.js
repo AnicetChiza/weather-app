740703e5346c99fc386d021301d2d5175877f0ec
unknown <chizaanicet@gmail.com>
2023-07-12 16:22:03 +0200

    add to my repository

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,7 @@
 const apiKey = "863242cfb2b1d357e6093d9a4df19a4b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const inputBox = document.querySelector('.input');
+const inputBox = document.querySelector('.search input');
 const inputBtn = document.querySelector('.search button');
 
 async function checkWeather(city){
