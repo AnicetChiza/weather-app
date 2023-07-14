42d3eed26a5cd0028b64cffa4efbd26559a05c02
unknown <chizaanicet@gmail.com>
2023-07-14 15:32:30 +0200

    add to my repository

--- Js/app.js
+++ Js/app.js
@@ -3,34 +3,33 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const inputBox = document.querySelector('.search input');
 const inputBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('weather-icon');
+const weatherIcon = document.querySelector('.weather-icon');
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
-
-    console.log(data);
 
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 
-    if(data.weather[0].main == 'clouds'){
+    if(data.weather[0].main == 'Clouds'){
         weatherIcon.src = 'images/clouds.png';
     }
-    else if(data.weather[0].main == 'rain'){
+    else if(data.weather[0].main == 'Rain'){
         weatherIcon.src = 'images/rain-icon.png';
     }
-    else if(data.weather[0].main == 'clear'){
+    else if(data.weather[0].main == 'Clear'){
         weatherIcon.src = 'images/clear-icon.png';
     }
-    else if(data.weather[0].main == 'drizzle'){
+    else if(data.weather[0].main == 'Drizzle'){
         weatherIcon.src = 'images/drizzle.png';
     }
-    else if(data.weather[0].main == 'snow'){
+    else if(data.weather[0].main == 'Snow'){
         weatherIcon.src = 'images/snow.png';
     }
+    document.querySelector('.weather').style.display = 'block';
 
 }
 
